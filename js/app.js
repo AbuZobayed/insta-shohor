a95652e5ce8c92fd753f77024d8abdb95185c035
AbuZobayed <zobayed.ict05@gmail.com>
2022-03-10 22:50:15 +0600

    user photo show

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -162,7 +162,7 @@ const loadPosts = async () =>{
   let data = await fetch('../data/posts.json');
   posts = await data.json();
   showPosts(posts);
-  console.log(showPosts);
+  console.log(posts);
 }
 
 loadPosts();
